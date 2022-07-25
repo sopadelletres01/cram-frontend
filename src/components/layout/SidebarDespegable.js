869a4f4d4d6af06f4f1465af37fd185167da4d52
cristina <cristinaaran89@gmail.com>
2022-07-25 09:56:50 +0200

    [SOLAMENTE FALTA QUE APAREZCA EL LOG OUT CUANDO ESTE LOGUEADOgit add .]

--- src/components/layout/SidebarDespegable.js
+++ src/components/layout/SidebarDespegable.js
@@ -1,39 +1,38 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import { FaUser } from "react-icons/fa";
+import { FaUser , FaPercent,FaTheaterMasks, FaPowerOff} from "react-icons/fa";
 
 function SidebarDespegable({ show, setShow }) {
   const domeNode = useRef();
-//   const [sidebarClicked, setSidebarClicked] = useState(false);
-//   const btn = document.getElementById("showSidebarBtn");
-//   const updateState = (event) => {
-//     console.log("updateState");
-//     //   console.log(event.target)
-//     if (buttonClicked) {
-//       setShow(!show);
-//       return;
-//     }
-//     if (domeNode.current.contains(event.target)) {
-//       setSidebarClicked(true);
-//       return;
-//     } else {
-//       setSidebarClicked(false);
-//       setShow(!show);
-//     }
-//   };
-//   useEffect(() => {
-//     document.addEventListener("mousedown", updateState);
-//     return () => {
-//       document.removeEventListener("mousedown", updateState);
-//     };
-//   }, []);
+
   return (
     <>
         <div className={`sidebar ${show ? 'active' : ''}`} ref={domeNode}>
           <Link className="sidebar__link" to="/user/profile">
             <FaUser />
             <span>Profile</span>
-          </Link>
+        </Link>
+        {/* Mostrar los eventos gratis si no estas logeado */}
+        <Link className="sidebar__link" to="/user/events">
+            <FaTheaterMasks />
+            <span>Eventos</span>
+        </Link>
+
+        <Link className="sidebar__link" to="/user/promotions">
+            <FaPercent />
+            <span>Promociones</span>
+        </Link>
+        {/* si estas logeado que aparezca el logout */}
+        
+          
+        <Link className="sidebar__link logout" to="/auth/logout">
+            <FaPowerOff />
+            <span>Logout</span>
+        </Link>
+        
+        
+
+        
           {/* link que abrace a un emoticotono narajna  y a las letras */}
         </div>
     </>
