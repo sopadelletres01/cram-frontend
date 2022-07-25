6cec1b091b2750528f2aefe757719a64640bf213
sopadelletres01 <marcportero01@gmail.com>
2022-07-25 12:44:40 +0200

    header updated

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,11 +5,17 @@ import { Link } from "react-router-dom";
 export default function Header({ className, showSidebar, ...rest }) {
   return (
     <header className={className ? className : ""}>
-      <Link to="/login">Iniciar Sesion</Link>
-
-      <button id="showSidebarBtn" onClick={showSidebar}>
-        <FaBars />
-      </button>
+      <div className="logoWrapper">
+        <Link to="/login">Iniciar Sesion</Link>
+      </div>
+      <div className="headerButtonsWrapper">
+        <Link to="/about">Sobre Nosotros</Link>
+        <Link to="/register">Sign Up</Link>
+        <Link to="/login">Login</Link>
+        <button id="showSidebarBtn" onClick={showSidebar}>
+          <FaBars />
+        </button>
+      </div>
 
       {rest.children}
     </header>
